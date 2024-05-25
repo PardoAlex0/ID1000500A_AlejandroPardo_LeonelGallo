fc8efaa933e70502514b10bad9ff42c3502ca559
PardoAlex0 <TAE2024.3@cinvestav.mx>
2024-05-24 18:46:14 -0600

    Modificacion driver python

--- driver/py/ID1000500A.py
+++ driver/py/ID1000500A.py
@@ -1,67 +1,187 @@
+import logging, time
 from ipdi.ip.pyaip import pyaip, pyaip_init
 
-import sys
+class convolution:
+    ## Class constructor of IP Convolution driver
+    #
+    # @param self Object pointer
+    # @param connector Middleware object
+    # @param nic_addr Network address IP
+    # @param port Port number
+    # @param csv_file Path to CSV configuration file
+    def __init__(self, conector, nic_addr, port, csv_file):
+        ## Pyaip object
+        self.__pyaip = pyaip_init(connector, nic_addr, port, csv_file)
+        
+        if self.__pyaip is None:
+            logging.debug(error)
+           
+        ## Array of strings with information read
+        self.dataRX = []
+        
+        ## IP Convolution IP-ID
+        self.IPID = 0
+        
+        self.__getID()
+        
+        self.__clearStatus()
+        
+        logging.debug(f"IP Convolution controller created with IP ID {self.IPID:08x}")
+        
+    ## Write data in the IP Convolution input memory
+    #
+    # @param self Object pointer
+    # @param data List with the data to write
+    def writeData(self, mem_name, data):
+        self.__pyaip.writeMem(mem_name, data, len(data), 0)
+        
+        logging.debug(f"Data written to {mem_name}")
+        
+    ## Read data from the IP output memory
+    #
+    # @param self Object pointer
+    # @param size Amount of data to read
+    def readData(self, mem_name, size):
+        data = self.__pyaip.readMem(mem_name, size, 0)
+        logging.debug(f"Data read from {mem_name}")
+        return data
+    
+    ## Start processing in IP
+    #
+    # @param self Object pointer
+    def startIP(self):
+        self.__pyaip.start()
 
-try:
+        logging.debug("Start sent")
+        
+    ## Write configuration register
+    #
+    # @param self Object pointer
+    # @param reg_name Configuration register name
+    # @param data Data to write
+    def writeConfReg(self, reg_name, data):
+        self.__pyaip.writeConfReg(reg_name, data, len(data), 0)
+        
+        logging.debug(f"Configuration register {reg_name} written with data {data}")
+        
+    ## Enable IP interruptions
+    #
+    # @param self Object pointer
+    def enableINT(self):
+        self.__pyaip.enableINT(0,None)
+
+        logging.debug("Int enabled")
+        
+    ## Disable IP interruptions
+    #
+    # @param self Object pointer
+    def disableINT(self):
+        self.__pyaip.disableINT(0)
+
+        logging.debug("Int disabled")
+        
+    ## Show IP status
+    #
+    # @param self Object pointer
+    def status(self):
+        status = self.__pyaip.getStatus()
+        logging.info(f"Status: {status:08x}")
+
+            
+    ## Finish connection
+    #
+    # @param self Object pointer
+    def finish(self):
+        self.__pyaip.finish()
+        logging.debug("Connection finished")
+        
+    ## Wait for the completion of the process
+    #
+    # @param self Object pointer
+    def waitInt(self):
+        waiting = True
+        
+        while waiting:
+
+            status = self.__pyaip.getStatus()
+
+            logging.debug(f"Status: {status:08x}")
+            
+            if status & 0x1:
+                waiting = False
+            
+            time.sleep(0.1)
+            
+    ## Get IP ID
+    #
+    # @param self Object pointer
+    def __getID(self):
+        self.IPID = self.__pyaip.getID()
+        
+    ## Clear status register of IP
+    #
+    # @param self Object pointer
+    def __clearStatus(self):
+        for i in range(8):
+            self.__pyaip.clearINT(i)
+            
+    ## Convolve two signals
+    #
+    # @param self Object pointer
+    # @param X First signal
+    # @param Y Second signal
+    # @return Convolution result
+    def conv(self, X, Y):
+        sizeX = len(X)
+        sizeY = len(Y)
+        size_aux = sizeX + sizeY - 1
+        result = [0] * size_aux
+        
+        for i in range(sizeX):
+            for j in range(sizeY):
+                result[i + j] += X[i] * Y[j]
+        
+        logging.debug(f"Convolution result: {result}")
+        return result
+    
+    
+if __name__=="__main__":
+    import sys, time, os
+    
+    logging.basicConfig(level=logging.INFO)
     connector = '/dev/ttyACM0'
-    nic_addr = 1
-    port = 0
     csv_file = '/home/a/Documents/HDL/ID1000500A_AlejandroPardo_LeonelGallo/config/ID1000500A_config.csv'
+    addr = 1
+    port = 0
+    
+    try:
+        driver = convolution(conector, addr, port, csv_file)
+        logging.info("Test Convolution: Driver created")
+    except:
+        logging.error("Test Convolution: Driver not created")
 
-    aip = pyaip_init(connector, nic_addr, port, csv_file)
-
-    aip.reset()
-
-    #==========================================
-    # Code generated with IPAccelerator 
-
-    ID = aip.getID()
-    print(f'Read ID: {ID:08X}\n')
-
-    STATUS = aip.getStatus()
-    print(f'Read STATUS: {STATUS:08X}\n')
-
-    MemX = [0x00000001, 0x00000002, 0x00000003, 0x00000004, 0x00000003, 0x00000007, 0x00000006, 0x0000000A, 0x00000005, 0x00000008]
-
-    print('Write memory: MdataX')
-    aip.writeMem('MdataX', MemX, 10, 0)
-    print(f'MemX Data: {[f"{x:08X}" for x in MemX]}\n')
-
-    MemY = [0x00000003, 0x00000003, 0x00000005, 0x00000006, 0x00000007]
-
-    print('Write memory: MdataY')
-    aip.writeMem('MdataY', MemY, 5, 0)
-    print(f'MemY Data: {[f"{x:08X}" for x in MemY]}\n')
-
-    Size = [0x000000AA]
-
-    print('Write configuration register: Csize')
-    aip.writeConfReg('Csize', Size, 1, 0)
-    print(f'Size Data: {[f"{x:08X}" for x in Size]}\n')
-
-    print('Start IP\n')
-    aip.start()
-
-    STATUS = aip.getStatus()
-    print(f'Read STATUS: {STATUS:08X}\n')
-
-    print('Read memory: MdataZ')
-    MemZ = aip.readMem('MdataZ', 14, 0)
-    print(f'MemZ Data: {[f"{x:08X}" for x in MemZ]}\n')
-
-    print('Clear INT: 0')
-    aip.clearINT(0)
-
-    STATUS = aip.getStatus()
-    print(f'Read STATUS: {STATUS:08X}\n')
-
-    #==========================================
-
-    aip.finish()
-
-except:
-    e = sys.exc_info()
-    print('ERROR: ', e)
-
-    aip.finish()
-    raise
+    ID = driver.getID()
+    logging.info(f'Read ID: {ID:08X}\n')
+    
+    STATUS = driver.getStatus()
+    logging.info(f'Read STATUS: {STATUS:08X}\n')
+    
+    X = [0x00000001, 0x00000002, 0x00000003, 0x00000004, 0x00000003, 0x00000007, 0x00000006, 0x0000000A, 0x00000005, 0x00000008]
+    driver.writeData('MdataX', X)
+    logging.info(f'TX MemX Data: {[f"{x:08X}" for x in X]}')
+    
+    Y = [0x00000003, 0x00000003, 0x00000005, 0x00000006, 0x00000007]
+    driver.writeData('MdataY', Y)
+    logging.info(f'TX MemY Data: {[f"{x:08X}" for x in Y]}')
+    
+    driver.startIP()
+    driver.waitInt()
+    
+    conv_result = driver.conv(X,Y)
+    logging.info(f'Convolution result: {[f"{x:08X}" for x in conv_result]}')
+    
+    driver.status()
+    driver.finish()
+    
+    logging.info("The End")
+    
